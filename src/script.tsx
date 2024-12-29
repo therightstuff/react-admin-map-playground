cb7b20b0d6dceaa62af58913bb5861a48cd12641
therightstuff <adam@industrialcuriosity.com>
2024-12-29 21:40:14 +0200

    initialized react admin

---
+++ src/script.tsx
@@ -0,0 +1,10 @@
+// this is a duplicate of index.tsx to get it to work in dev mode
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
